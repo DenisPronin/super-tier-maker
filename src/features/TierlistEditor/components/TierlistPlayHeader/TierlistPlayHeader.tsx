94e3db22825ca7dff5b9b5c43d6fe20649eb741e
DenisPronin <pronin.idenis@gmail.com>
2025-12-31 12:32:41 +0100

    Tierlist Editor - funny header

--- src/features/TierlistEditor/components/TierlistPlayHeader/TierlistPlayHeader.tsx
+++ src/features/TierlistEditor/components/TierlistPlayHeader/TierlistPlayHeader.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from '@mantine/core'
+import { Flex, Image, Text } from '@mantine/core'
 import {
   selectTierlist,
   useTierlistEditorStore,
@@ -7,11 +7,70 @@ import {
 export function TierlistPlayHeader() {
   const tierlist = useTierlistEditorStore(selectTierlist)
 
+  const leftImages = [
+    '/alf.gif',
+    '/brooklyn.gif',
+    '/sabrina.gif',
+    '/danger.gif',
+    '/danger.gif',
+    '/danger.gif',
+    '/danger.gif',
+    '/danger.gif',
+  ]
+
+  const rightImages = [
+    '/danger.gif',
+    '/danger.gif',
+    '/danger.gif',
+    '/danger.gif',
+    '/danger.gif',
+    '/danger.gif',
+    '/danger.gif',
+    '/danger.gif',
+  ]
+
+  const renderImages = (images: string[]) =>
+    images.map((src, index) => (
+      <Image
+        key={`${src}-${index}`}
+        src={src}
+        height={60}
+        fit="contain"
+        style={{ width: 'auto' }}
+      />
+    ))
+
   return (
-    <Box p="md" bg="dark.7">
-      <Text ta="center" size="xl" fw={700}>
+    <Flex
+      justify="center"
+      align="center"
+      gap="sm"
+      h="80px"
+      style={{ overflow: 'hidden' }}
+      mb="md"
+      mt="md"
+    >
+      <Flex gap="sm" style={{ overflow: 'hidden', flexShrink: 1 }}>
+        {renderImages(leftImages)}
+      </Flex>
+
+      <Text
+        size="xl"
+        style={{
+          fontSize: '3rem',
+          whiteSpace: 'nowrap',
+          flexShrink: 0,
+          fontWeight: 700,
+          marginLeft: '16px',
+          marginRight: '16px',
+        }}
+      >
         {tierlist.data?.title}
       </Text>
-    </Box>
+
+      <Flex gap="sm" style={{ overflow: 'hidden', flexShrink: 1 }}>
+        {renderImages(rightImages)}
+      </Flex>
+    </Flex>
   )
 }
